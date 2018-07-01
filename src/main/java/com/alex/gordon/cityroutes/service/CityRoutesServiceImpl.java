53e5a6a392e7b4c4aeaf46be3e5ca26ca8a20f52
Alex Gordon <Alex.Gordon.gbs@gmail.com>
2018-07-01 19:17:49 -0400

    Initial commit - project skeleton created.

---
+++ src/main/java/com/alex/gordon/cityroutes/service/CityRoutesServiceImpl.java
@@ -0,0 +1,14 @@
+package com.alex.gordon.cityroutes.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CityRoutesServiceImpl implements CityRoutesService {
+
+	@Override
+	public String connected(String origin, String destination) {
+		// TODO Auto-generated method stub
+		return "Not implemented yet!";
+	}
+
+}

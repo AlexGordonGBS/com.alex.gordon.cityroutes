996c0b7fe295535f21bfceb4f82448753624bbe3
Alex Gordon <Alex.Gordon.gbs@gmail.com>
2018-07-05 23:32:05 -0400

    Unit and Integration Tests
    Units test for the service layer.
    Integration tests for the controller layer using MockMvc. Code coverage
    - over 90%.

--- src/test/java/com/alex/gordon/cityroutes/ApplicationTests.java
+++ src/test/java/com/alex/gordon/cityroutes/ApplicationTests.java
@@ -3,16 +3,23 @@ package com.alex.gordon.cityroutes;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 /**
- * <p>A typical test application class for the SpringBoot application </p/ 
+ * <p>
+ * A typical test application class for the SpringBoot application </p/
+ * 
  * @author Alex Gordon
  *
  */
 public class ApplicationTests {
+
+	protected MockRestServiceServer mockRestServer;
 
 	@Test
 	public void contextLoads() {
--- src/test/java/com/alex/gordon/cityroutes/service/CityRoutesServiceImplTest.java
+++ src/test/java/com/alex/gordon/cityroutes/service/CityRoutesServiceImplTest.java
@@ -2,6 +2,7 @@ package com.alex.gordon.cityroutes.service;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -18,7 +19,10 @@ import com.alex.gordon.cityroutes.util.DataFileLoader;
 
 @RunWith(MockitoJUnitRunner.class)
 /**
- * <p> Test class example for servie class </p>
+ * <p>
+ * Test class example for servie class
+ * </p>
+ * 
  * @author Alex Gordon
  *
  */
@@ -59,6 +63,112 @@ public class CityRoutesServiceImplTest {
 		String actual = service.connected(origin, destination);
 
 		assertTrue(expected.equals(actual));
+	}
+
+	/**
+	 * test method for the service layer for one particular test case when the route does NOT exist
+	 */
+	@Test
+	public void connectedTest2() {
+		Map<String, Set<String>> map = new HashMap<>();
+		Set<String> set = new HashSet<>();
+		set.add("a");
+		set.add("b");
+		map.put("x", set);
+		set = new HashSet<>();
+		set.add("x");
+		map.put("a", set);
+
+		doReturn(map).when(loader).buildRoutesMap();
+
+		String origin = "x";
+		String destination = "z";
+		String expected = "no";
+
+		String actual = service.connected(origin, destination);
+
+		assertTrue(expected.equals(actual));
+	}
+
+	/**
+	 * test method for the service layer for one particular test case when the INDIRECT route does exist
+	 * ==> recursive part of the algorithm would be called
+	 */
+	@Test
+	public void connectedTest3() {
+		Map<String, Set<String>> map = new HashMap<>();
+
+		// imitate route x==>a==>b==>c==d==>e
+		// thus x==>e
+		Set<String> set = new HashSet<>();
+		set.add("a");
+		map.put("x", set);
+		set = new HashSet<>();
+		set.add("x");
+		map.put("a", set);
+
+		set = new HashSet<>();
+		set.add("b");
+		map.put("a", set);
+		set = new HashSet<>();
+		set.add("a");
+		map.put("b", set);
+
+		set = new HashSet<>();
+		set.add("b");
+		map.put("c", set);
+		set = new HashSet<>();
+		set.add("c");
+		map.put("b", set);
+
+		set = new HashSet<>();
+		set.add("c");
+		map.put("d", set);
+		set = new HashSet<>();
+		set.add("d");
+		map.put("c", set);
+
+		set = new HashSet<>();
+		set.add("d");
+		map.put("e", set);
+		set = new HashSet<>();
+		set.add("e");
+		map.put("d", set);
+
+		doReturn(map).when(loader).buildRoutesMap();
+
+		String origin = "a";
+		String destination = "e";
+		String expected = "yes";
+
+		String actual = service.connected(origin, destination);
+
+		assertTrue(expected.equals(actual));
+	}
+
+	/**
+	 * test method for the service layer when an error occures in loader
+	 */
+	@Test(expected = RuntimeException.class)
+	public void connectedTest_error() {
+		Map<String, Set<String>> map = new HashMap<>();
+
+		// imitate route x==>a==>b==>c==d==>e
+		// thus x==>e
+		Set<String> set = new HashSet<>();
+		set.add("a");
+		map.put("x", set);
+		set = new HashSet<>();
+		set.add("x");
+		map.put("a", set);
+
+		doThrow(new RuntimeException()).when(loader).buildRoutesMap();
+
+		String origin = "a";
+		String destination = "x";
+		String expected = "yes";
+
+		String actual = service.connected(origin, destination);
 	}
 
 }
